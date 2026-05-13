d01357278a6600a122e1857b4f3362c267783404
njlmrvllr <anjaliamarovillar@outlook.com>
2026-05-13 12:08:07 -0500

    feat: refactor ShoppingCartCommandServiceImpl and ShoppingCartRepository to extend JpaRepository

--- src/main/java/com/smartcart/shoppingjourney/infrastructure/persistence/jpa/repositories/ShoppingCartRepository.java
+++ src/main/java/com/smartcart/shoppingjourney/infrastructure/persistence/jpa/repositories/ShoppingCartRepository.java
@@ -1,5 +1,10 @@
 package com.smartcart.shoppingjourney.infrastructure.persistence.jpa.repositories;
 
-public interface ShoppingCartRepository {
+import com.smartcart.shoppingjourney.domain.model.entities.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    // Aquí es donde va el "extends JpaRepository" y el tipo "Long" (en mayúscula)
 }
